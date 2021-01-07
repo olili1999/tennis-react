3ff43220753512647e18ed3350062f55e4e99cb9
oliver li <olili@umich.edu>
2021-01-06 23:33:14 -0500

    added left part of profile page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,25 @@
 import React from 'react'; 
 
 
+export default function SignUp() { 
+  return (
+    <> 
+      <div> 
+      <h3> Sign Up </h3>  
 
-function SignUp() { 
-  return (<div> <h3> SignUp </h3>  </div>);  
+      </div>
+    </> 
+    
+    );  
 }
 
-export default SignUp; 
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="/__/firebase/8.2.1/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="/__/firebase/8.2.1/firebase-analytics.js"></script>
+
+// <!-- Initialize Firebase -->
+// <script src="/__/firebase/init.js"></script>nz
